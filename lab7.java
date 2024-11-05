27b516f8608fbd58669c9451c923ecd68dff5a77
shandanaktk <sammy39998@gmail.com>
2024-11-05 17:08:35 +0500

    Second commit

--- lab7.java
+++ lab7.java
@@ -2,5 +2,6 @@ public class lab7{
 public static void main(String[] args)
 {
     System.out.println("First commit by Shandana, Samar, Ahmad and Mahi");
+    System.out.println("Second commit by Shandana, Samar, Ahmad and Mahi");
 }
 }
